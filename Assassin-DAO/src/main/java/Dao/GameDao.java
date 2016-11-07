359cf3ff49619ec6e5767f64adfabd70bee8c50f
Chris <you@example.com>
2016-11-06 23:44:10 -0500

    some info about table creation

--- Assassin-DAO/src/main/java/Dao/GameDao.java
+++ Assassin-DAO/src/main/java/Dao/GameDao.java
@@ -4,4 +4,5 @@ package Dao;
  * Created by Darargon on 11/6/16.
  */
 public class GameDao {
+    //CREATE TABLE games
 }
--- Assassin-DAO/src/main/java/Dao/UserDao.java
+++ Assassin-DAO/src/main/java/Dao/UserDao.java
@@ -4,4 +4,11 @@ package Dao;
  * Created by Darargon on 11/6/16.
  */
 public class UserDao {
+    /**CREATE TABLE users
+     * (
+     * id INTEGER
+     * name VARCHAR(SIZE)
+     * picture VARCHAR(SIZE)
+     * )
+     */
 }
