9512ea590f677e3734e2f1085e20b54f50963829
Christopher <bekreth@gmail.com>
2016-11-25 19:12:43 -0500

    Project can now be built.

---
+++ Assassin-Rest/src/main/java/ApplicationConfig.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by bekreth on 11/25/16.
+ */
+@SpringBootApplication
+public class ApplicationConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+}
--- Assassin-Rest/src/main/java/endpoints/implmentation/PlayerController.java
+++ Assassin-Rest/src/main/java/endpoints/implmentation/PlayerController.java
@@ -8,14 +8,14 @@ import models.api.responses.playerapi.GameStateResponse;
 import models.api.responses.playerapi.KillResponse;
 import models.api.responses.playerapi.MyTargetResponse;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * An implementation of the the PlayerApi interface
  */
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class PlayerController implements PlayerApi {
 
